2796ede84c43adde8f1efc1112756dcf1260c25d
Denis Kozub <denikozub@gmail.com>
2022-06-05 16:16:13 +0300

    bert_encoder

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from torch import nn
 
 from dataset import TwitterData
 from dataset import Dataset  # for pickle
-from model import BertFF, BertAE
+from model import BertFF, BertAE, BertEncoderFF
 from trainer import train, evaluate, geodesic_distance
 
 
@@ -31,6 +31,20 @@ def train_bert_ae():
           batch_size=64,
           AE=True)
     evaluate(bert_autoencoder, dataset.test_data, batch_size=64, criterion=geodesic_distance, AE=True)
+
+def train_bert_encoder_ff():
+    dataset = TwitterData()
+    encoder = BertAE()
+    encoder.load('models/BertEncoder_10.pt')
+    bert_encoder_ff = BertEncoderFF(encoder, encoder.size())
+    train(bert_encoder_ff,
+          dataset.train_data,
+          dataset.val_data,
+          learning_rate=1e-3,
+          criterion=geodesic_distance,
+          epochs=10,
+          batch_size=64e)
+    evaluate(bert_encoder_ff, dataset.test_data, batch_size=64, criterion=geodesic_distance)
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -59,7 +59,28 @@ class BertAE(nn.Module):
         return self.decoder(self.encoder(noised_output)), pooled_output if decode else self.encoder(pooled_output)
 
     def save(self, epoch_num: int) -> None:
-        torch.save(self.encoder.state_dict(), path.join('models', f'BertAE_{epoch_num}.pt'))
+        torch.save(self.encoder.state_dict(), path.join('models', f'BertEncoder_{epoch_num}.pt'))
 
     def load(self, epoch_num: int) -> None:
-        self.encoder.load_state_dict(torch.load(path.join('models', f'BertAE_{epoch_num}.pt')))
+        self.encoder.load_state_dict(torch.load(path.join('models', f'BertEncoder_{epoch_num}.pt')))
+    
+    def size():
+        return 64
+
+
+class BertEncoderFF(nn.Module):
+
+    def __init__(self, encoder, encoder_size):
+        self.bert = BertModel.from_pretrained('bert-base-cased')
+        self.encoder = encoder
+        self.linear = nn.Linear(encoder_size, 2)
+    
+    def forward(self, input_id, mask, decode=True, factor=0.01):
+        _, pooled_output = self.bert(input_ids=input_id, attention_mask=mask, return_dict=False)
+        return self.linear(self.encoder(pooled_output))
+
+    def save(self, epoch_num: int) -> None:
+        torch.save(self.state_dict(), path.join('models', f'BertEncoderFF_{epoch_num}.pt'))
+
+    def load(self, epoch_num: int) -> None:
+        self.load_state_dict(torch.load(path.join('models', f'BertEncoderFF_{epoch_num}.pt')))
